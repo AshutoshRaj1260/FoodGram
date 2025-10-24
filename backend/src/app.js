c22c8b1e07f8019e75fbe0556587d20331de4938
AshutoshRaj1260 <ashutoshrajofficial@gmail.com>
2025-10-24 12:08:40 +0530

    No Info

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL.replace(/\/$/, ""),
     credentials: true,
 }))
 
