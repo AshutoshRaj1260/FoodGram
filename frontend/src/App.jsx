0360f27b6f810ce71ddd6ed989a3679c8c51a8f3
Deepika Kumari <onlydeepikakumari@gmail.com>
2026-05-16 01:00:07 +0530

    Updated  controled app and main file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import './App.css'
-import AppRoutes from './routes/AppRoutes.jsx'
-
+import React from "react";
+import "./App.css";
+import AppRoutes from "./routes/AppRoutes.jsx";
 
 function App() {
-
   return (
     <>
-    <AppRoutes />
+      <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import './styles/variables.css'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <StrictMode>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </StrictMode>
 )
