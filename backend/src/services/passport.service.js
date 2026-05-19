feaf465f555b610643fe23c0953a4e31c56e56bd
Rakshita Kumari <rakshita0903@gmail.com>
2026-05-19 20:02:02 +0530

    fix: make Google OAuth initialization conditional on env vars

--- backend/src/services/passport.service.js
+++ backend/src/services/passport.service.js
@@ -1,32 +1,37 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const userModel = require("../models/user.model");
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        const email = profile.emails[0].value;
-        let user = await userModel.findOne({ email });
-        if (!user) {
-          user = await userModel.create({
-            fullName: profile.displayName,
-            email,
-            password: null, //OAuth users dont require password
-          });
+if (
+  process.env.GOOGLE_CLIENT_ID &&
+  process.env.GOOGLE_CLIENT_SECRET &&
+  process.env.GOOGLE_CALLBACK_URL
+) {
+  const GoogleStrategy = require("passport-google-oauth20").Strategy;
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const email = profile.emails[0].value;
+          let user = await userModel.findOne({ email });
+          if (!user) {
+            user = await userModel.create({
+              fullName: profile.displayName,
+              email,
+              password: null,
+            });
+          }
+          return done(null, user);
+        } catch (error) {
+          return done(error, null);
         }
-        return done(null, user); //null - no error
-
-      } catch (error) {
-        return done(error, null); 
       }
-    }
-  )
-);
+    )
+  );
+}
 
 module.exports = passport;
